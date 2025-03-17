2f65cc5fc8f4a4469e1c04463b981a86a99dde48
Christopher Louët <christopher.louet@pm.me>
2025-03-17 22:26:11 +0100

    Adding hadolint linter and trivy scan

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ COPY pom.xml .
 COPY src src
 
 RUN ./mvnw install -DskipTests
-RUN mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+WORKDIR /workspace/app/target/dependency
+RUN jar -xf ../*.jar
 
 FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
--- run.sh
+++ run.sh
@@ -4,28 +4,29 @@
 CURRENT_PATH="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 DC_FILE="$CURRENT_PATH/docker-compose.yml"
 DC_PROD_FILE="$CURRENT_PATH/docker-compose-prod.yml"
+IMAGE_NAME="kalon:latest"
 
-# Shell coloring
-function colors() {
-    case $1 in
-        'R') echo -e '\033[0;31m' ;; # RED
-        'G') echo -e '\033[0;32m' ;; # GREEN
-        'B') echo -e '\033[0;34m' ;; # BLUE
-        'Y') echo -e '\033[0;33m' ;; # YELLOW
-        'W') echo -e '\033[0;37m' ;; # WHITE
-        'N') echo -e '\033[0;0m' ;; # NOCOLOR
-    esac
-}
+ARGS=()
+HELP=0 VERBOSE=0
+PROD=0 UP=0 DOWN=0 BUILD=0 LINT=0
 
-function info() {
-    echo -e "$(colors 'W')$*$(colors 'N')"
-    return 0
-}
+source "$CURRENT_PATH/libs/utils.sh"
 
-function debug() {
-    if [[ $((VERBOSE)) -gt 0 ]]; then
-        echo -e "$(colors 'B')$*$(colors 'N')"
-    fi
+function display_help() {
+    local output=""
+    output="
+$(colors 'Y')Usage$(colors 'W') $(basename "$0") [OPTIONS] COMMAND
+$(colors 'Y')Commands:$(colors 'N')
+$(colors 'G')up$(colors 'W')                  Initialize and creating containers
+$(colors 'G')down$(colors 'W')                Stopping/removing containers, networks and volumes
+$(colors 'G')build$(colors 'W')               Building docker image
+$(colors 'G')lint$(colors 'W')                Use hadolint to lint the Dockerfile
+$(colors 'Y')Options:$(colors 'N')
+$(colors 'G')-p, --prod$(colors 'W')          Starting kalon container
+$(colors 'G')-v, --verbose$(colors 'W')       Make the command more talkative
+$(colors 'G')-h, --help$(colors 'W')          Display help
+    "
+    echo -e "$output\n"|sed '1d; $d'
     return 0
 }
 
@@ -61,27 +62,30 @@ function build() {
     info "Build Docker image"
     debug "docker compose -f $DC_PROD_FILE build"
     ! docker compose -f "$DC_PROD_FILE" build && return 1
+    if [ -z "$(docker images -q $IMAGE_NAME 2> /dev/null)" ]; then
+      error "Docker image failed to be created"
+      return 2
+    fi
+    ! scan_trivy && return 1
     return 0
 }
 
-function display_help() {
-    local output=""
-    output="
-$(colors 'Y')Usage$(colors 'W') $(basename "$0") [OPTIONS] COMMAND
-$(colors 'Y')Commands:$(colors 'N')
-$(colors 'G')up$(colors 'W')                  Initialize and creating containers
-$(colors 'G')down$(colors 'W')                Stopping/removing containers, networks and volumes
-$(colors 'G')build$(colors 'W')               Building docker image
-$(colors 'Y')Options:$(colors 'N')
-$(colors 'G')-p, --prod$(colors 'W')          Starting kalon container
-$(colors 'G')-v, --verbose$(colors 'W')       Make the command more talkative
-$(colors 'G')-h, --help$(colors 'W')          Display help
-    "
-    echo -e "$output\n"|sed '1d; $d'
+function scan_trivy() {
+    info "Execute a scan to find vulnerabilities"
+    debug "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.18.3 $IMAGE_NAME"
+    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.18.3 $IMAGE_NAME
+    return 0
+}
+
+function lint_dockerfile() {
+    info "Run hadolint"
+    debug "docker run --rm -i hadolint/hadolint < Dockerfile"
+    ! docker run --rm -i hadolint/hadolint < Dockerfile && return 1
     return 0
 }
 
 # Check options passed as script parameters.
+# shellcheck disable=SC2034
 function check_opts() {
     read -ra opts <<< "$@"
     for opt in "${opts[@]}"; do
@@ -89,6 +93,7 @@ function check_opts() {
             up) UP=1 ; ARGS+=("$opt") ;;
             down) DOWN=1 ; ARGS+=("$opt") ;;
             build) BUILD=1 ; ARGS+=("$opt") ;;
+            lint) LINT=1 ;;
             --prod|-p) PROD=1 ;;
             --verbose|-v) VERBOSE=1 ;;
             --help) HELP=1 ;;
@@ -96,41 +101,35 @@ function check_opts() {
         esac
     done
     # Help is displayed if no option is passed as script parameter
-    if [[ $((HELP+UP+DOWN+BUILD)) -eq 0 ]]; then
+    if [[ $((HELP+UP+DOWN+BUILD+LINT)) -eq 0 ]]; then
         HELP=1
     fi
     return 0
 }
 
 function main() {
-    ARGS=()
-    local HELP=0 VERBOSE=0
-    local PROD=0 UP=0 DOWN=0 BUILD=0
     # Check options passed as script parameters and execute tasks
     ! check_opts "$@" && return 1
-    # Execute one or more tasks according to script parameters
-    ! execute_tasks && return $?
-    return 0
-}
-
-# Execute tasks based on script parameters or user actions.
-function execute_tasks() {
     # Display help
     if [[ $HELP -gt 0 ]]; then
-        ! display_help && return 1
+        ! display_help && return 2
         return 0
     fi
     # Starting Docker containers
     if [[ $UP -gt 0 ]]; then
-        ! up && return 2
+        ! up && return 3
     fi
     # Stopping and remove containers, networks and volumes
     if [[ $DOWN -gt 0 ]]; then
-        ! down && return 3
+        ! down && return 4
     fi
     # Building docker image
     if [[ $BUILD -gt 0 ]]; then
-        ! build && return 4
+        ! build && return 5
+    fi
+    # Lint Dockerfile
+    if [[ $LINT -gt 0 ]]; then
+        ! lint_dockerfile && return 6
     fi
     return 0
 }

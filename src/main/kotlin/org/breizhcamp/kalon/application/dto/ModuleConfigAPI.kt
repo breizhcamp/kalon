69e081c4d7464b2e6592886a2ebc99b6901b845f
Floris Bartra <floris.bartra@gmail.com>
2026-07-22 11:09:21 +0200

    [config] Add auth config in sponsor-front config

--- src/main/kotlin/org/breizhcamp/kalon/application/dto/ModuleConfigAPI.kt
+++ src/main/kotlin/org/breizhcamp/kalon/application/dto/ModuleConfigAPI.kt
@@ -19,7 +19,12 @@ fun List<Pair<String, String>>.toModuleConfigAPI(): List<ModuleConfigAPI> =
 
 fun ModuleConfig.toApi(): List<ModuleConfigAPI> = when (this) {
     is OrgaModuleConfig -> this.auth.toApi() + this.backends.flatMap { it.toApi() }
-    is SponsorModuleConfig -> this.backends.flatMap { it.toApi() }
+    is SponsorModuleConfig -> listOf(
+        ModuleConfigAPI(
+            "DEFAULT_EVENT_ID",
+            this.defaultEventId.value,
+        ),
+    ) + this.auth.toApi() + this.backends.flatMap { it.toApi() }
 }
 
 fun ModuleAuthConfig.toApi(): List<ModuleConfigAPI> = listOf(
